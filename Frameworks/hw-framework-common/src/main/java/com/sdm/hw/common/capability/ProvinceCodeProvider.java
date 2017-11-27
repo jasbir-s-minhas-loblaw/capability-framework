a6686adb9ac64943b793c4aafbcaa187a0bb6b82
Jasbir Minhas <jasbir.s.minhas@gmail.com>
2017-11-27 18:15:10 -0500

    Made minor changes.

--- Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/ProvinceCodeProvider.java
+++ Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/ProvinceCodeProvider.java
@@ -1,5 +1,13 @@
 package com.sdm.hw.common.capability;
 
+import com.sdm.hw.common.capability.db.StorePreferenceManager;
+import com.sdm.hw.common.capability.db.entity.StorePreference;
+import org.hibernate.annotations.SourceType;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
+
 /**
  * This class provides the current province
  */
@@ -31,5 +39,29 @@ public final class ProvinceCodeProvider {
 
     void setCurrentProvinceCode(ProvinceCode provinceCode){
         this.provinceCode = provinceCode;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
+        StorePreferenceManager storePreferenceManager =
+                applicationContext.getBean("storePreferenceManager", StorePreferenceManager.class);
+
+        System.out.println(">> Printing Province ID");
+        List<StorePreference> storePreferences = storePreferenceManager.findAll();
+        printStorePreferences(storePreferences);
+        System.out.println(">> Done.");
+    }
+
+    private static void printStorePreferences(List<StorePreference> storePreferences) {
+        if (storePreferences != null) {
+            for (StorePreference storePreference : storePreferences) {
+                System.out.println(storePreference);
+            }
+        } else {
+            System.out.println("Null StorePreferences returned.");
+        }
+
     }
 }
