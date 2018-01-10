e9321fa7f05516545fdf5e5a2d0fa5fdc6381574
jasbir-s-minhas-loblaw <33465741+jasbir-s-minhas-loblaw@users.noreply.github.com>
2018-01-10 15:22:16 -0500

    Took changes made by Vivek and incorporate in the master branch.

--- Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/CapabilityBooleanKey.java
+++ Frameworks/hw-framework-common/src/main/java/com/sdm/hw/common/capability/CapabilityBooleanKey.java
@@ -46,99 +46,124 @@ package com.sdm.hw.common.capability;
  * @since 2017-11-07
  */
 public enum CapabilityBooleanKey implements CapabilityKey {
-    // the following enum is for testing multilevel of groups with boolean capability during development process.
-    TEST_2_LEVEL_GROUP("testGroup1L1.testGroup1L2.testBooleanCapability"),
-
-    //eHealth Status
-    // Note: eHealth expression ends with a "." which indicates that it is a group
-    EHEALTH_ENABLED("eHealth."),
-    //View ADR Status
-    ADR_STATUS("eHealth.adrStatus"),
-    //View replaced version status Changes for Allergy/Intolerance
-    ALLERGY_REPLACED_VERSION("eHealth.allergyReplacedVersion"),
+    //Sub-eHealth Status
+    // Note: Sub-eHealth expression ends with a "." which indicates that it is a group
+	EHEALTH("eHealth."),
+    //ePrescribing Enabled Check
+    EPRESCRIBING("eHealth.ePrescribing"),
+    //Rx Order Id Enabled Check
+    PRINT_RX_ORDER_ID("eHealth.printRxOrderId"),
+    //Manage Consent Enabled Check
+    MANAGE_CONSENT("eHealth.manageConsent"),
+    //Add DIS Provincial Condition
+    ADD_DIS_PROVINCIAL_CONDITION("eHealth.addDISProvincialCondition"),
+    //View Reason For Access Popup
+    REASON_TO_ACCESS_POPUP("eHealth.reasonToAccessPopup"),
+    //Link to Provincial Rx
+    LINK_PROVINCIAL_RX("eHealth.linkProvincialRx"),
+    //View Status Change
+    VIEW_STATUS_CHANGE("eHealth.viewStatusChange"),
+    //Trigger DIS ONOK
+    TRIGGER_DIS_ONOK("eHealth.triggerDISONOK"),
     //Allergy Status Drop Down Option
     ALLERGY_STATUS("eHealth.allergyStatus"),
-    //Sub E-health configurable capability to check whether DIN exists for the entered chemical or not
-    CHEMICAL_DIN_CHECK("eHealth.chemicalDINCheck"),
+    //Intolerance Status Dropdown Option
+    INTOL_STATUS("eHealth.intolStatus"),
+    //View ADR Status
+    ADR_STATUS("eHealth.adrStatus"),
+    //Store Province Option
+    STORE_PROVINCE_OPTION("eHealth.storeProvinceOption"),
+    //Enable Provincial Patient Search On Client Registry
+    PATIENT_SEARCH("eHealth.patientSearch"),
+    //Provincial Patient Linking Is Required Or Not
+    PATIENT_LINKAGE("eHealth.patientLinkage"),
+    //Non-Fill Actions on Provincial Rx require Intake Enabled
+    PROVINCIAL_RX_NON_FILL_ACTIONS_REQUIRE_INTAKE("eHealth.provincialRxNonFillActionsRequireIntake"),
+    //There is a sub-ehealth configurable capability mode in HW (On/Off) to make the patient profile review
+    //mandatory during CV.
+    PATIENT_PROFILE_REVIEW_IS_MANDATORY_AT_CV("eHealth.patientProfileReviewIsMandatoryAtCV"),
+    //Display Management History At CV
+    DISPLAY_MANAGEMENT_HISTORY_AT_CV("eHealth.displayManagementHistoryAtCV"),
+    //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the Tx of selected Provincial
+    DISPLAY_TX_FOR_PROVINCIAL_RX("eHealth.displayTxForProvincialRx"),
     //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the comprehensive profile to
     // be user selectable or mandatory.
     COMPREHENSIVE_MODIFIER("eHealth.comprehensiveModifier"),
-    //View replaced version status Changes for medical condition
-    CONDITION_REPLACED_VERSION("eHealth.conditionReplacedVersion"),
-    //DECN Business entity type value
-    DECN_BUSINESS_ENTITY_TYPE("eHealth.decnBusinessEntityType"),
-    //TODO: Add comments.
+    //Display Provincial Data
     DISPLAY_PROVINCIAL_DATA("eHealth.displayProvincialData"),
-    //TODO: Add comments.
-    DISPLAY_MANAGEMENT_HISTORY_AT_CV("eHealth.displayManagementHistoryAtCv"),
-    //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the Tx of selected Provincial
-    //Rx on Rx Table to be user viewable or not.
-    DISPLAY_TX_FOR_PROVINCIAL_RX("eHealth.displayTxForProvincialRx"),
-    //TODO: Add comments.
-    ELIGIBLE_FOR_TRIAL("eHealth.eligibleForTrial"),
-    //ePrescribing Enabled Check
-    EPRESCRIBING("eHealth.ePrescribing"),
-    //There is a sub-ehealth configurable capability mode in HW (On/Off) to enable importing
-    //provincial prescriptions.
-    IMPORT_PROVINCIAL_PRESCRIPTIONS("eHealth.importProvincialPrescriptions"),
-    //Intolerance Status Dropdown Option
-    INTOL_STATUS("eHealth.intolStatus"),
-    //Link to Provincial Rx
-    LINK_PROVINCIAL_RX("eHealth.linkProvincialRx"),
-    //Enable Link to Prov Indicator
-    LINK_STATUS_INDICATOR("eHealth.linkStatusIndicator"),
-    //Provincial Location Search Enabled Check
-    LOCATION_SEARCH("eHealth.locationSearch"),
-    //Manage Consent Enabled Check
-    MANAGE_CONSENT("eHealth.manageConsent"),
-    //Provincial Patient Linking Is Required Or Not
-    PATIENT_LINKAGE("eHealth.patientLinkage"),
-    //There is a sub-ehealth configurable capability mode in HW (On/Off) to make the patient profile review
-    //mandatory during CV.
-    PATIENT_PRO_REV_IS_MANDATORY_AT_CV("eHealth.patientProRevIsMandatoryAtCV"),
-    //Enable Provincial Patient Search On Client Registry
-    PATIENT_SEARCH("eHealth.patientSearch"),
-    //TODO: Add comments.
-    PHONE_ENABLED("eHealth.phoneEnabled"),
-    //TODO: Add comments.
-    POSTAL_CODE_ENABLED("eHealth.postalCodeEnabled"),
-    //Rx Order Id Enabled Check
-    PRINT_RX_ORDER_ID("eHealth.printRxOrderId"),
-    //There must be a sub-ehealth configuration capability (ON/OFF) in HW to allow the system to execute the
-    //Producer ID check when linking provincial patient to local.
-    PRODUCER_ID_CHECK("eHealth.producerIDCheck"),
-    //Provincial Provider Search Enabled Check
-    PROVIDER_SEARCH("eHealth.providerSearch"),
-    //Non-Fill Actions on Provincial Rx require Intake Enabled
-    PROVINCIAL_RX_REQUIRE_INTAKE("eHealth.provincialRxRequireIntake"),
     //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the medication profile
     //to be user selectable or not.-->
     PROV_MEDICATION_PROFILE("eHealth.provMedicationProfile"),
-    //View Provincial Observation
-    QUERY_PROVINCIAL_OBSERVATIONS("eHealth.queryProvincialObservations"),
-    //View Reason For Access Popup
-    REASON_TO_ACCESS_POPUP("eHealth.reasonToAccessPopup"),
     //check for  Provincial Note
+    // Start BP 350 Changes
     SEND_PATIENT_NOTE("eHealth.sendPatientNote"),
-    //Configuration Capability to disable/enable the not sent report from Action menu of Exception Queue.
-    SEND_PROVINCIAL_NOT_SENT_REPORT("eHealth.sendProvincialNotSentReport"),
-    //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the user to select rx/tx note
-    // to be sent to province from Data entry screen.
-    SEND_RX_TX_NOTES("eHealth.sendRxTxNotes"),
-    //View Provincial Allergies is Mandatory
-    VIEW_ALLERGIES_MANDATORY("eHealth.viewAllergiesMandatory"),
-    //Sub-Ehealth View Provincial ADRs
-    VIEW_PROVINCIAL_ADRS("eHealth.viewProvincialAdrs"),
-    //Sub-Ehealth View Provincial Allergies-->
-    VIEW_PROVINCIAL_ALLERGIES("eHealth.viewProvincialAllergies"),
+    // End BP 350 Changes
+    //Provincial Provider Search Enabled Check
+    // Start code EHR1.BP.310 for Query DIS Prescriber Record.
+    PROVIDER_SEARCH("eHealth.providerSearch"),
+    // End code EHR1.BP.310 for Query DIS Prescriber Record.
+    //Provincial Location Search Enabled Check
+    //Start code EHR1.BP.320 for Query DIS Location Record.
+    LOCATION_SEARCH("eHealth.locationSearch"),
+    //End code EHR1.BP.320 for Query DIS Location Record.
+    //View Provincial Observation
+    // Start BP 580 Changes
+    QUERY_PROVINCIAL_OBSERVATIONS("eHealth.queryProvincialObservations"),
+    // End BP 580 Changes
     //Sub-Ehealth View Provincial Reactions
     VIEW_PROVINCIAL_REACTIONS("eHealth.viewProvincialReactions"),
+    //Sub-Ehealth View Provincial Allergies-->
+    VIEW_PROVINCIAL_ALLERGIES("eHealth.viewProvincialAllergies"),
     //View Provincial Patient Conditions
     VIEW_PROV_MEDICAL_CONDITIONS("eHealth.viewProvMedicalConditions"),
-    //View Provincial Patient Conditions
-    VIEW_STATUS_CHANCE("eHealth.viewStatusChance"),
-    //TODO: Add comments.
-    UNDISC_RX_ACTION("eHealth.undiscRxAction");
+    //View Provincial Allergies is Mandatory
+    VIEW_ALLERGIES_MANDATORY("eHealth.viewAllergiesMandatory"),
+    //View replaced version status Changes for medical condition
+    CONDITION_REPLACED_VERSION("eHealth.conditionReplacedVersion"),
+    //Sub-Ehealth View Provincial ADRs
+    VIEW_PROVINCIAL_ADRS("eHealth.viewProvincialAdrs"),
+    //View replaced version status Changes for Allergy/Intolerance
+    ALLERGY_REPLACED_VERSION("eHealth.allergyReplacedVersion"),
+    //Phone Enabled
+    PHONE_ENABLED("eHealth.phoneEnabled"),
+    //Postal Code Enabled
+    POSTAL_CODE_ENABLED("eHealth.postalCodeEnabled"),
+    //Sub E-health configurable capability to check whether DIN exists for the entered chemical or not
+    CHEMICAL_DIN_CHECK("eHealth.chemicalDINCheck"),
+    //Transfer Province Location Id
+    //Changes for Transfer screen enhancement SPrint 3 NS
+    TRANSFER_PROVINCE_LOCATION_ID("eHealth.transferProvinceLocationId"),
+    //Configuration Capability to disable/enable the not sent report from Action menu of Exception Queue.
+    // start - Code changes for BP 640 Request DIS Provide Sent/Not Sent Report - Release- 16.2
+    SEND_PROVINCIAL_NOT_SENT_REPORT("eHealth.sendProvincialNotSentReport"),
+    // end - Code changes for BP 640 Request DIS Provide Sent/Not Sent Report - Release- 16.2
+    //There is a sub-ehealth configurable capability mode in HW (On/Off)  for the user to select rx/tx note
+    // to be sent to province from Data entry screen.
+    // start - Code changes for BP 350 Add Rx Tx Notes - Release- 16.2
+    SEND_RX_TX_NOTES("eHealth.sendRxTxNotes"),
+    // end - Code changes for BP 350 Add Rx Tx Notes - Release- 16.2
+    //There is a sub-ehealth configurable capability mode in HW (On/Off) to enable importing
+    //provincial prescriptions.
+    // Start code changes for BP 2000 Import Provincial Rx Release 16.2 on 29/04/2016
+    IMPORT_PROVINCIAL_PRESCRIPTIONS("eHealth.importProvincialPrescriptions"),
+    // End code changes for BP 2000 Import Provincial Rx Release 16.2 on 29/04/2016
+    //There must be a sub-ehealth configuration capability (ON/OFF) in HW to allow the system to execute the
+    //Producer ID check when linking provincial patient to local.
+    // Start - Code changes for BP 240 Configuration added Release-16.2 on 26/05/2016
+    PRODUCER_ID_CHECK("eHealth.producerIDCheck"),
+    // End - Code changes for BP 240 Configuration added Release-16.2 on 26/05/2016
+    //Enable Link to Prov Indicator
+    // Start - Code changes for Link To Prov. Indicator Configuration added Release-16.2 on 05/08/2016
+    LINK_STATUS_INDICATOR("eHealth.linkStatusIndicator"),
+    // End - Code changes for Link To Prov. Indicator Configuration added Release-16.2 on 05/08/2016
+    //Undisc Rx Action
+    // Start - Code changes for CR 77
+    UNDISC_RX_ACTION("eHealth.undiscRxAction"),
+    // End - Code changes for CR 77
+    //HW_COPY_PATIENT_CONFIG("HW_COPY_PATIENT_CONFIG"),
+    //Eligible For Trial
+    ELIGIBLE_FOR_TRIAL("eHealth.eligibleForTrial");
+
 
     /**
      * a String representing a path to the capability
