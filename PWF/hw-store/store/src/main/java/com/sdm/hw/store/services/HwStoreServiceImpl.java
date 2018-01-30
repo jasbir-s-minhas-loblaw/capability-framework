1fa97a56bcec43c0988b6b2e09d0841850a5548e
Jasbir Minhas <jasbir.minhas@loblaw.ca>
2018-01-30 01:45:13 -0500

    Added @depricated java doc as suggested by batch.

--- PWF/hw-store/store/src/main/java/com/sdm/hw/store/services/HwStoreServiceImpl.java
+++ PWF/hw-store/store/src/main/java/com/sdm/hw/store/services/HwStoreServiceImpl.java
@@ -4,6 +4,12 @@ import com.sdm.hw.store.dto.SubEHealthConfigConstants;
 import com.sdm.hw.exception.services.HwBaseAppException;
 
 public class HwStoreServiceImpl {
+    /**
+     * @deprecated (Should pass enum rather than passing a string)
+     * @param capabilityName
+     * @return
+     * @throws HwBaseAppException
+     */
     @Deprecated
     public boolean isSubEHealthEnabled(String capabilityName)
             throws HwBaseAppException {
