a9acdf242abd6c30f49bcd1a198b00cae9e7e6ea
Jasbir Minhas <jasbir.minhas@loblaw.ca>
2018-01-30 02:16:49 -0500

    Fixed code coverage by fixing Jacoco line.

--- sonar.sh
+++ sonar.sh
@@ -2,5 +2,6 @@
 DATE=`date '+%Y-%m-%dT%H:%M:%S%z'`
 VERSION=`date '+%Y-%m-%d_%H-%M-%S'`
 echo $DATE
-mvn clean install  -Dplugin=org.jacoco:jacoco-maven-plugin -Ddetail
+#mvn clean install  -Dplugin=org.jacoco:jacoco-maven-plugin -Ddetail
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install -Dmaven.test.failure.ignore=false
 mvn sonar:sonar -Dsonar.host.url=http://localhost:9000 -Dsonar.projectVersion=$VERSION -Dsonar.projectDate=$DATE
